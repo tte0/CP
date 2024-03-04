c5b1d5ef70bc6773c5afa5560449b50b5c1b7a05
DESKTOP-1U1I7GD\teoata <teoata17@gmail.com>
2024-03-04 15:27:57 +0300

    .

---
+++ J_Sushi.cpp
@@ -0,0 +1,100 @@
+/*
+MIT License
+
+Copyright (c) 2024 tte0 (teomana,teoata17)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#pragma GCC optimize("O3,fast-math")
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+#define endl '\n'
+#define spc ' '
+#define pb push_back
+#define e2(x) (1LL<<(x))
+#define gcd(x,y) __gcd(x,y)
+#define lcm(x,y) ((x*y)/gcd(x,y))
+#define smrt(i) (double(sqrt(8*(i)+1)-1)/2)
+#define ssum(x) ((x)*((x)+1)/2)
+#define isint(x) (ceil((x))==floor((x)))
+#define no cout<<"NO"<<endl
+#define yes cout<<"YES"<<endl
+#define cendl cout<<endl
+#define mset(x,y) memset(x,y,sizeof(x))
+#define popcnt(x) __builtin_popcountll(x)
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cout<<fixed<<setprecision(10)
+#define fileio freopen("out.put","w",stdout);freopen("in.put","r",stdin)
+using namespace std;
+typedef int_fast64_t ll;
+typedef long double ldouble;
+typedef string str;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vi> vvi;
+typedef vector<pair<char,int>> vci;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<str,int> msi;
+typedef set<int> sti;
+typedef set<char> stc;
+typedef set<str> sts;
+typedef multiset<int> msti;
+typedef multiset<char> mstc;
+typedef multiset<str> msts;
+const int N=200005;
+const int MOD=1000000007;
+const ll  INF=4e18;
+const double PI=4*atan(1);
+inline int fp(int b,int p,int mod=MOD){int ans=1;while(p){if(p&1)ans=(ans*b)%mod;p>>=1;b=(b*b)%mod;}return ans;}
+///////////////////////////////////////////////////////////////////
+int n,m,k,t,q,a,b,x,y,ans;
+vi v;
+
+inline double f(int a,int b,int c,vector<vector<vector<double>>>& dp){
+    if(a==0 && b==0 && c==0)return 0;
+    if(a<0 || b<0 || c<0)return 0;
+    if(dp[a][b][c]!=0)return dp[a][b][c];
+    return dp[a][b][c]=(n+a*f(a-1,b,c,dp)+b*f(a+1,b-1,c,dp)+c*f(a,b+1,c-1,dp))/(a+b+c);
+}
+
+inline void solve(void){
+    cin>>n;
+    int a=0,b=0,c=0;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        if(x==1)a++;
+        if(x==2)b++;
+        if(x==3)c++;
+    }
+    vector<vector<vector<double>>> dp(n+1,vector<vector<double>>(n+1,vector<double>(n+1,0)));
+    cout<<f(a,b,c,dp);
+}
+
+int32_t main(void){
+    fastio;
+    t=1;
+    //cin>>t;
+    while(t--)solve();
+}
