9cadbe7f8fa6f42b507875c2370a4a1038d40838
Teoman Ata Korkmaz <teoata17@gmail.com>
2024-03-26 18:53:41 +0300

    .

--- Hotel_Queries.cpp
+++ Hotel_Queries.cpp
@@ -1,50 +1,37 @@
 /*
-MIT License with Attribution and Non-Commercial Use Restriction
+MIT License
 
-Copyright (c) 2024 tte0 (aka. teomana,teoata17)
+Copyright (c) 2024 tte0 (teomana,teoata17)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to
-deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the Software
-is furnished to do so, subject to the following conditions:
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-1. The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-2. If you use this Software, you must give appropriate attribution by
-prominently displaying the original author's name.
-
-3. If you modify the Software, you must clearly indicate that you have
-modified the Software and include the original author's name.
-
-4. This Software may not be used for commercial purposes, including, but
-not limited to, selling the Software or using the Software in a way that generates income.
-
-THE SOFTWARE IS PROVIDED "AS IS," WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
-OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT
-OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 #pragma GCC optimize("O3,fast-math")
 #include <bits/stdc++.h>
-#define int int_fast64_t
+#define int ll
 #define ff first
 #define ss second
 #define endl '\n'
 #define spc ' '
 #define pb push_back
-#define N (200005)
-#define MOD (int(1e9)+7)
-#define MOD2 (998244353)
-#define MODL (int(1e9)+21)
-#define INF (int(4e18))
 #define e2(x) (1LL<<(x))
 #define gcd(x,y) __gcd(x,y)
-#define lcm(x,y) ((x*y)/gcd(x,y))
+#define lcm(x,y) ((x/gcd(x,y))*y)
 #define smrt(i) (double(sqrt(8*(i)+1)-1)/2)
 #define ssum(x) ((x)*((x)+1)/2)
 #define isint(x) (ceil((x))==floor((x)))
@@ -52,12 +39,14 @@ OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTW
 #define yes cout<<"YES"<<endl
 #define cendl cout<<endl
 #define mset(x,y) memset(x,y,sizeof(x))
+#define popcnt(x) __builtin_popcountll(x)
 #define all(x) x.begin(),x.end()
 #define rall(x) x.rbegin(),x.rend()
-#define dbg(x) cdebug()<<debug(x)
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cout<<fixed<<setprecision(0)
+#define fileio freopen("out.put","w",stdout);freopen("in.put","r",stdin)
+#define usacoio(s) freopen((s + str(".in")).c_str(), "r", stdin);freopen((s + str(".out")).c_str(), "w", stdout)
 using namespace std;
-typedef long long ll;
+typedef int_fast64_t ll;
 typedef long double ldouble;
 typedef string str;
 typedef pair<int,int> ii;
@@ -66,6 +55,7 @@ typedef vector<int> vi;
 typedef vector<ii> vii;
 typedef vector<iii> viii;
 typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
 typedef vector<pair<char,int>> vci;
 typedef map<int,int> mii;
 typedef map<char,int> mci;
@@ -76,41 +66,58 @@ typedef set<str> sts;
 typedef multiset<int> msti;
 typedef multiset<char> mstc;
 typedef multiset<str> msts;
-/////////////////////////////////////////////////////////////
-int n,m,k,t,q,x,y,ans,st_value[4*N],st_ind[4*N];
+const int N=200005;
+const int MOD=1000000007;
+const ll  INF=4e18;
+const double PI=4*atan(1);
+inline int fp(int b,int p,int mod=MOD){int ans=1;while(p){if(p&1)ans=(ans*b)%mod;p>>=1;b=(b*b)%mod;}return ans;}
+///////////////////////////////////////////////////////////////////
+int n,m,k,t,q,a,b,x,y,ans,st[2*N];
 vi v={0};
 
-inline void build(int node,int l,int r){
-    if(l==r){
-        st_value[node]=v[l];
-        st_ind[node]=l;
-        return;
-    }
-    int mid=(l+r)/2;
-    build(node*2,l,mid);
-    build(node*2+1,mid+1,r);
-    st_value[node]=max(st_value[node*2],st_value[node*2+1]);
-    if(st_value[2*node]>=st_value[2*node+1])st_ind[node]=st_ind[2*node];
-    else st_ind[node]=st_ind[2*node+1];
+#define mid ((l+r)/2)
+
+inline int build(int l,int r,int node){
+    if(l==r)return st[node]=v[l];
+    return st[node]=max(build(l,mid,2*node),build(mid+1,r,2*node+1));
 }
 
-inline int query(int node,int l,int r,int x){
-    if(st[node]<x)return 0;
+inline int query(int l,int r,int node){
+    if(st[node]<x)return INF;
+    if(l==r)return l;
+    return min(query(l,mid,2*node),query(mid+1,r,2*node+1));
+}
+
+inline int update(int l,int r,int node,int t){
+    if(t<l || r<t)return st[node];
     if(l==r){
-        st_value[node]=-=x;
-        return l;
+        st[node]-=t;
+        return st[node];
+    }
+    return st[node]=max(update(l,mid,2*node,t),update(mid+1,r,2*node+1,t));
+}
+
+inline void solve(void){
+    cin>>n>>q;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        v.pb(x);
+    }
+    build(1,n,1);
+    //cerr<<"st: ";for(int i=0;i<2*n;i++)cerr<<st[i]<<spc;cerr<<endl;
+
+    while(q--){
+        cin>>x;
+        int t=query(1,n,1);
+        t=(t==INF?0:t);
+        cout<<t<<" ";
+        if(t)update(1,n,1,t);
+
+        //cerr<<"st: ";for(int i=0;i<2*n;i++)cerr<<st[i]<<spc;cerr<<endl;
     }
 }
 
 int32_t main(void){
-    cin>>n>>q;
-    for(int i=0,i<n,i++){
-        cin>>x;
-        v.pb(x);
-    }
-    build(1,1,n);
-    while(q--){
-        cin>>x;
-        cout<<query(1,1,n,x)<<endl;
-    }
+    fastio;
+    solve();
 }
