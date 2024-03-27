34c3bc497049452e7e9661a3debc6426a3bf9391
Teoman Ata Korkmaz <teoata17@gmail.com>
2024-03-27 20:48:21 +0300

    .

--- High_Score.cpp
+++ High_Score.cpp
@@ -1,33 +1,25 @@
 /*
-MIT License with Attribution and Non-Commercial Use Restriction
+MIT License
 
-Copyright (c) 2024 tte0 (aka. teomana,teoata17)
+Copyright (c) 2024 tte0 (teomana,teoata17)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to
-deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the Software
-is furnished to do so, subject to the following conditions:
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-1. The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-2. If you use this Software, you must give appropriate attribution by
-prominently displaying the original author's name.
-
-3. If you modify the Software, you must clearly indicate that you have
-modified the Software and include the original author's name.
-
-4. This Software may not be used for commercial purposes, including, but
-not limited to, selling the Software or using the Software in a way that generates income.
-
-THE SOFTWARE IS PROVIDED "AS IS," WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
-OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT
-OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 #pragma GCC optimize("O3,fast-math")
 #include <bits/stdc++.h>
@@ -37,14 +29,9 @@ OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTW
 #define endl '\n'
 #define spc ' '
 #define pb push_back
-#define N (200005)
-#define MOD (int(1e9)+7)
-#define MOD2 (998244353)
-#define MODL (int(1e9)+21)
-#define INF (int(4e18))
 #define e2(x) (1LL<<(x))
 #define gcd(x,y) __gcd(x,y)
-#define lcm(x,y) ((x*y)/gcd(x,y))
+#define lcm(x,y) ((x/gcd(x,y))*y)
 #define smrt(i) (double(sqrt(8*(i)+1)-1)/2)
 #define ssum(x) ((x)*((x)+1)/2)
 #define isint(x) (ceil((x))==floor((x)))
@@ -52,12 +39,14 @@ OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTW
 #define yes cout<<"YES"<<endl
 #define cendl cout<<endl
 #define mset(x,y) memset(x,y,sizeof(x))
+#define popcnt(x) __builtin_popcountll(x)
 #define all(x) x.begin(),x.end()
 #define rall(x) x.rbegin(),x.rend()
-#define dbg(x) cdebug()<<debug(x)
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cout<<fixed<<setprecision(0)
+#define fileio freopen("out.put","w",stdout);freopen("in.put","r",stdin)
+#define usacoio(s) freopen((s + str(".in")).c_str(), "r", stdin);freopen((s + str(".out")).c_str(), "w", stdout)
 using namespace std;
-typedef long long ll;
+typedef int_fast64_t ll;
 typedef long double ldouble;
 typedef string str;
 typedef pair<int,int> ii;
@@ -66,6 +55,7 @@ typedef vector<int> vi;
 typedef vector<ii> vii;
 typedef vector<iii> viii;
 typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
 typedef vector<pair<char,int>> vci;
 typedef map<int,int> mii;
 typedef map<char,int> mci;
@@ -76,66 +66,22 @@ typedef set<str> sts;
 typedef multiset<int> msti;
 typedef multiset<char> mstc;
 typedef multiset<str> msts;
-/////////////////////////////////////////////////////////////
-int n,m,k,t,q,x,y,w,ans,dist[2505],bt[2505],arr[2505],vis[N];
-vi v,radj[N],adj[N];
-viii edges;
+const int N=200005;
+const int MOD=1000000007;
+const ll  INF=4e18;
+const double PI=4*atan(1);
+inline int fp(int b,int p,int mod=MOD){int ans=1;while(p){if(p&1)ans=(ans*b)%mod;p>>=1;b=(b*b)%mod;}return ans;}
+///////////////////////////////////////////////////////////////////
+int n,m,k,t,q,a,b,x,y,ans;
+vi v;
 
-inline void dfs(int node){
-    if(vis[node]++)return;
-    arr[node]|=2;
-    for(int i:adj[node])dfs(i);
-    return;
-}
-inline void dfs2(int node){
-    if(vis[node]++)return;
-    arr[node]|=4;
-    for(int i:radj[node])dfs2(i);
-    return;
+inline void solve(void){
+    cin>>n;
 }
 
 int32_t main(void){
     fastio;
-    cin>>n>>m;
-    for(int i=1;i<=n;i++)dist[i]=(1==i?0:INF);
-    for(int i=0;i<m;i++){
-        cin>>x>>y>>w;
-        adj[x].pb(y);
-        radj[y].pb(x);
-        edges.pb({-w,{x,y}});
-    }
-    for(int i=0;i<n-1;i++){
-        for(auto i:edges){
-            w=i.ff;
-            x=i.ss.ff;
-            y=i.ss.ss;
-            if(dist[y]>dist[x]+w){
-                dist[y]=dist[x]+w;
-            }
-        }
-        //cerr<<"dist: ";for(int i=1;i<=n;i++)cerr<<dist[i]<<" ";cerr<<endl;
-    }
-    
-    
-    for(auto i:edges){
-        w=i.ff;
-        x=i.ss.ff;
-        y=i.ss.ss;
-        if(dist[y]>dist[x]+w){
-            dist[y]=dist[x]+w;
-            arr[y]|=1;
-        }
-        //cerr<<"dist2: ";for(int i=1;i<=n;i++)cerr<<dist[i]<<" ";cerr<<endl;
-    }
-    dfs(1);
-    mset(vis,0);
-    dfs2(n);
-    for(int i=1;i<=n;i++){
-        if(arr[i]==7){
-            cout<<-1;
-            return 0;
-        }
-    }
-    
-    cout<<-dist[n];
+    t=1;
+    //cin>>t;
+    while(t--)solve();
 }
