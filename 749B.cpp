b76afb8ad72c721a159cd9981fcddbfd2b79b5a2
Teoman Ata Korkmaz <teoata17@gmail.com>
2024-04-08 12:06:41 +0300

    .

---
+++ 749B.cpp
@@ -0,0 +1,98 @@
+/*
+MIT License
+
+Copyright (c) 2024 tte0 (teomana,teoata17)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#pragma GCC optimize("O3,fast-math")
+#include <bits/stdc++.h>
+#define int ll
+#define ff first
+#define ss second
+#define endl '\n'
+#define spc ' '
+#define pb push_back
+#define e2(x) (1LL<<(x))
+#define gcd(x,y) __gcd(x,y)
+#define lcm(x,y) ((x/gcd(x,y))*y)
+#define smrt(i) (double(sqrt(8*(i)+1)-1)/2)
+#define ssum(x) ((x)*((x)+1)/2)
+#define isint(x) (ceil((x))==floor((x)))
+#define no cout<<"NO"<<endl
+#define yes cout<<"YES"<<endl
+#define cendl cout<<endl
+#define mset(x,y) memset(x,y,sizeof(x))
+#define popcnt(x) __builtin_popcountll(x)
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cout<<fixed<<setprecision(0)
+#define fileio freopen("out.put","w",stdout);freopen("in.put","r",stdin)
+#define usacoio(s) freopen((s + str(".in")).c_str(), "r", stdin);freopen((s + str(".out")).c_str(), "w", stdout)
+#define wait while(clock()-start<=0.585*CLOCKS_PER_SEC);
+using namespace std;
+typedef int_fast64_t ll;
+typedef long double ldouble;
+typedef string str;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<pair<char,int>> vci;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<str,int> msi;
+typedef set<int> sti;
+typedef set<char> stc;
+typedef set<str> sts;
+typedef multiset<int> msti;
+typedef multiset<char> mstc;
+typedef multiset<str> msts;
+const int N=200005;
+const int MOD=1000000007;
+const ll  INF=4e18;
+const double PI=4*atan(1);
+inline int fp(int b,int p,int mod=MOD){int ans=1;while(p){if(p&1)ans=(ans*b)%mod;p>>=1;b=(b*b)%mod;}return ans;}
+///////////////////////////////////////////////////////////////////
+int n,m,k,t,q,a,b,x,y,w,ans;
+vi v,adj[N];
+
+inline void solve(void){
+    int x1,x2,x3,y1,y2,y3;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+    set<ii> st;
+    st.insert({x3+(x1-x2),y3+(y1-y2)});
+    st.insert({x3+(x2-x1),y3+(y2-y1)});
+    st.insert({x2+(x3-x1),y2+(y3-y1)});
+    st.insert({x2+(x1-x3),y2+(y1-y3)});
+    st.insert({x1+(x2-x3),y1+(y2-y3)});
+    st.insert({x1+(x3-x2),y1+(y3-y2)});
+    cout<<st.size()<<endl;
+    for(auto i:st)cout<<i.ff<<spc<<i.ss<<endl;
+}
+
+int32_t main(void){
+    fastio;
+    t=1;
+    //cin>>t;
+    while(t--)solve();
+}
