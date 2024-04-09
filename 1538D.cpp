c6c8f1db72359af7493e8582f9cb597605bc53f1
Teoman Ata Korkmaz <teoata17@gmail.com>
2024-04-09 22:26:14 +0300

    .

--- 1538D.cpp
+++ 1538D.cpp
@@ -1,4 +1,27 @@
-#pragma GCC optimize("O3,fast-math")
+/*
+MIT License
+
+Copyright (c) 2024 tte0 (teomana,teoata17)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#pragma GCC optimize("O3,fast-math,unroll-all-loops")
 #include <bits/stdc++.h>
 #define int ll
 #define ff first
@@ -6,14 +29,8 @@
 #define endl '\n'
 #define spc ' '
 #define pb push_back
-#define N (200005)
-#define MOD (int(1e9)+7)
-#define MOD2 (998244353)
-#define MODL (int(1e9)+21)
-#define INF (int(4e18))
 #define e2(x) (1LL<<(x))
-#define gcd(x,y) __gcd(x,y)
-#define lcm(x,y) ((x*y)/gcd(x,y))
+#define lcm(x,y) ((x/gcd(x,y))*y)
 #define smrt(i) (double(sqrt(8*(i)+1)-1)/2)
 #define ssum(x) ((x)*((x)+1)/2)
 #define isint(x) (ceil((x))==floor((x)))
@@ -21,12 +38,15 @@
 #define yes cout<<"YES"<<endl
 #define cendl cout<<endl
 #define mset(x,y) memset(x,y,sizeof(x))
+#define popcnt(x) __builtin_popcountll(x)
 #define all(x) x.begin(),x.end()
 #define rall(x) x.rbegin(),x.rend()
-#define dbg(x) cdebug()<<debug(x)
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cout<<fixed<<setprecision(0)
+#define fileio freopen("out.txt","w",stdout);freopen("in.txt","r",stdin)
+#define usacoio(s) freopen((s + str(".in")).c_str(), "r", stdin);freopen((s + str(".out")).c_str(), "w", stdout)
+#define wait while(clock()-start<=0.585*CLOCKS_PER_SEC);
 using namespace std;
-typedef long long ll;
+typedef int_fast64_t ll;
 typedef long double ldouble;
 typedef string str;
 typedef pair<int,int> ii;
@@ -35,6 +55,7 @@ typedef vector<int> vi;
 typedef vector<ii> vii;
 typedef vector<iii> viii;
 typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
 typedef vector<pair<char,int>> vci;
 typedef map<int,int> mii;
 typedef map<char,int> mci;
@@ -45,28 +66,49 @@ typedef set<str> sts;
 typedef multiset<int> msti;
 typedef multiset<char> mstc;
 typedef multiset<str> msts;
-/////////////////////////////////////////////////////////////
-int n,m,k,t,q,x,y,a,b,c,ans,sieve[N];
+const int N=32000;
+const int MOD=1000000007;
+const ll  INF=4e18;
+const double PI=4*atan(1);
+inline int fp(int b,int p,int mod=MOD){int ans=1;while(p){if(p&1)ans=(ans*b)%mod;p>>=1;b=(b*b)%mod;}return ans;}
+///////////////////////////////////////////////////////////////////
+int n,m,k,t,q,p,a,b,x,y,w,ans,sieve[N];
 vi primes;
 
-inline void initializeSieve(){
-    mset(sieve,-1);
-    sieve[0]=sieve[1]=0;
-    for(int i=2;i<200000;i++){
-        if(sieve[i]==0)continue;
-        sieve[i]=1;
+inline void sieveinit(){
+    for(int i=2;i<N;i++){
+        if(sieve[i])continue;
         primes.pb(i);
-        for(int j=i*i;j<200000;j+=i)sieve[j]=0;
+        for(int j=i*i;j<N;j+=i)sieve[j]=1;
     }
 }
 
 inline void solve(void){
-    cin>>x>>y>>k;
-    if(gcd(x,y)==max(x,y))
+    cin>>a>>b>>k;
+    if(k==1 && (gcd(a,b)!=min(a,b) || a==b))return void(no);
+
+    int cnt_a=0,cnt_b=0;
+    for(const int& p:primes){
+        if(a==1 && b==1)break;
+        while(a%p==0){
+            cnt_a++;
+            a/=p;
+        }
+        while(b%p==0){
+            cnt_b++;
+            b/=p;
+        }
+    }
+    cnt_a+=(a>1);
+    cnt_b+=(b>1);
+    if(k>cnt_a+cnt_b)return void(no);
+    yes;
 }
 
 int32_t main(void){
-    initializeSieve();
+    sieveinit();
+    fastio;
+    t=1;
     cin>>t;
     while(t--)solve();
 }
--- template.cpp
+++ template.cpp
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#pragma GCC optimize("O3,fast-math")
+#pragma GCC optimize("O3,fast-math,unroll-all-loops")
 #include <bits/stdc++.h>
 #define int ll
 #define ff first
