268852d8753b1e6e7fe66ce79ae5a848d57cd3b2
Teoman Ata Korkmaz <teoata17@gmail.com>
2024-04-23 18:20:17 +0300

    .

---
+++ Problem_1_MooTube.cpp
@@ -0,0 +1,129 @@
+/*
+MIT License
+
+Copyright (c) 2024 tte0 (teomana,teoata17)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#pragma GCC optimize("O3,fast-math,unroll-all-loops")
+#include <bits/stdc++.h>
+#define int ll
+#define ff first
+#define ss second
+#define endl '\n'
+#define spc ' '
+#define pb push_back
+#define e2(x) (1LL<<(x))
+#define gcd(x,y) __gcd(x,y)
+#define lcm(x,y) ((x/gcd(x,y))*y)
+#define smrt(i) (double(sqrt(8*(i)+1)-1)/2)
+#define ssum(x) ((x)*((x)+1)/2)
+#define isint(x) (ceil((x))==floor((x)))
+#define no cout<<"NO"<<endl
+#define yes cout<<"YES"<<endl
+#define cendl cout<<endl
+#define mset(x,y) memset(x,y,sizeof(x))
+#define popcnt(x) __builtin_popcountll(x)
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define compress(x) sort(all(x));x.resize(unique(all(x))-x.begin())
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cout<<fixed<<setprecision(0)
+#define fileio freopen("out.txt","w",stdout);freopen("in.txt","r",stdin)
+#define usacoio(s) freopen((s + str(".in")).c_str(), "r", stdin);freopen((s + str(".out")).c_str(), "w", stdout)
+#define Ey_Turk_gencligi__Birinci_vazifen__bu_definei_kodunun_sonuna_eklemendir  clock_t start=clock();while(clock()-start<=0.585*CLOCKS_PER_SEC)
+using namespace std;
+typedef int32_t i32;
+typedef int_fast64_t ll;
+typedef long double ldouble;
+typedef string str;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<pair<char,int>> vci;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<str,int> msi;
+typedef set<int> sti;
+typedef set<char> stc;
+typedef set<str> sts;
+typedef multiset<int> msti;
+typedef multiset<char> mstc;
+typedef multiset<str> msts;
+const int N=100005;
+const int MOD=1000000007;
+const i32 INF=INT32_MAX;
+const ll  INFL=INT64_MAX;
+const double PI=4*atan(1);
+inline int fp(int b,int p,int mod=MOD){int ans=1;while(p){if(p&1)ans=(ans*b)%mod;p>>=1;b=(b*b)%mod;}return ans;}
+///////////////////////////////////////////////////////////////////
+int n,m,k,t,q,a,b,x,y,w,fa[N],ans[N];
+viii query,edges;
+
+inline int dsu(int x){
+    return (fa[x]<0?x:fa[x]=dsu(fa[x]));
+}
+
+inline void unionSet(int x,int y){//x->y
+    if(fa[dsu(x)]<fa[dsu(y)])swap(x,y); // 
+    fa[dsu(y)]+=fa[dsu(x)];
+    fa[dsu(x)]=dsu(y);
+}
+
+inline void solve(void){
+    mset(fa,-1);
+    cin>>n>>q;
+    for(int i=0;i<n-1;i++){
+        cin>>x>>y>>w;
+        x--,y--;
+        edges.pb({w,{x,y}});
+    }
+    sort(rall(edges));
+    for(int i=0;i<q;i++){
+        cin>>x>>y;
+        y--;
+        query.pb({x,{y,i}});
+    }
+    sort(rall(query));
+
+    int p=0;
+    for(auto _:query){
+        int k=_.ff,x=_.ss.ff,ind=_.ss.ss;
+        //cerr<<"query: "<<k<<","<<x+1<<","<<ind<<endl;
+        while(p<n-1 && edges[p].ff>=k){
+            unionSet(edges[p].ss.ff,edges[p].ss.ss);
+            p++;
+        }
+        ans[ind]=-fa[dsu(x)];
+        //cerr<<"fa: ";for(int i=0;i<n;i++)cerr<<(fa[i]<0?fa[i]:fa[i]+1)<<spc;cerr<<endl;
+    }
+
+    for(int i=0;i<q;i++)cout<<ans[i]-1<<endl;
+}
+
+i32 main(void){
+    fastio;
+    usacoio("mootube");
+    t=1;
+    //cin>>t;
+    while(t--)solve();
+}
