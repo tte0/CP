d8d5d4a83a6a586cb8e7353ff5eb81412e60859c
Teoman Ata Korkmaz <teoata17@gmail.com>
2024-08-11 10:03:26 +0300

    .

--- codegolf.cpp
+++ codegolf.cpp
@@ -1,8 +1,2 @@
-#include<stdio.h>
-int i;
-int main(){
-    for(int cnt=0,x=i;i++<1e3;){
-        while(x=(x&1?3*x+1:x/2)^1)cnt++;
-        printf("%d\n",cnt);
-    }
-}
+#include<cstdio>
+int i,j,c,x;int f(int x){return x?x%10+f(x/10):0;}int main(){for(;i++<1e4;c=0){for(j=1,x=i;++j<i;)for(;x%j==0;c+=f(j))x/=j;f(i)^c?:printf("%d\n",i);}}
