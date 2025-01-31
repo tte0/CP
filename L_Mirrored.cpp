278bcbc4ba725f053b4b3754a552ee7c77b4285a
Teoman Ata Korkmaz <teoata17@gmail.com>
2025-01-31 09:03:38 +0300

    .

--- L_Mirrored.cpp
+++ L_Mirrored.cpp
@@ -1,46 +1,49 @@
 // Author: Teoman Ata Korkmaz
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 #define int int_fast64_t
 using namespace std;
-/////////////////////////////////////////////////////////// max 18 dig
-int n,fp[21];
 
-inline int g(int i,int d,int x){
-    if(d-i-1<=i)return !x;
-    int ans=0;
-    int b=fp[d-i-1]-fp[i];
-    if(x/b>9)return 0;
-    ans=(10-x/b-!i)*g(i+1,d,x%b)+(10-(x/b+1)-!i)*g(i+1,d,abs(x%b-b));
-    return ans;
+int n, fp[21];
+
+inline int g(int i, int d, int x) {
+    // number of possible answers when:
+    // the number has "d" digits,
+    // processed "i" digits from left & right
+    // "x" amount left to produce diff
+
+    // possible answer when:
+    // there is no digits to process 
+    // no diff to produce
+    if (d <= 2 * i + 1) return !x;
+
+    // coefficent for the digit
+    int b = fp[d - i - 1] - fp[i];
+
+    //max difference can be made with digits is 9
+    if (x / b > 9) return 0;
+
+
+    int under_shoot = (10 - x / b - !i) * g(i + 1, d, x % b);
+    int over_shoot = (10 - (x / b + 1) - !i) * g(i + 1, d, abs(x % b - b));
+
+    return under_shoot + over_shoot;
 }
 
-inline int f(int d){
-    //cerr<<"f:"<<left<<setw(2)<<d<<"|";
-    /*int ans=1,x=n;
-    for(int i=0;d-i-1>i;i++){
-        int b=fp[d-i-1]-fp[i];
-        if(x/b>9)return 0;
-        ans*=10-x/b-!i;
-        x%=b;
-        //cerr<<ans<<",";
-    }
-    if(x)return 0;
-    if(d&1)ans*=10;
-    return ans;*/
-    int ans=g(0,d,n);
-    if(d&1)ans*=10;
-    return ans;
-}
+signed main() {
+    cin >> n;
 
-signed main(void){
-    fp[0]=1;for(int i=1;i<21;i++)fp[i]=fp[i-1]*10;
-    cin>>n;
+    fp[0] = 1;
+    for (int i = 1; i < 21; i++) //10s powers
+        fp[i] = fp[i - 1] * 10;
 
-    int ans=0;
-    for(int i=1;i<=20;i++){
-        ans+=f(i);  
-        //cerr<<"|"<<ans<<endl;
+    int ans = 0;
+    // calculating for all possible NoD 
+    for (int i = 1; i <= 20; i++) {
+        // one free digit when NoD is odd
+        int factor = (i & 1) ? 10 : 1;
+
+        ans += g(0, i, n) * factor;
     }
 
-    cout<<ans<<endl;
+    cout << ans << endl;
 }
--- NOI23_inspections.cpp
+++ NOI23_inspections.cpp
@@ -1,28 +1,69 @@
 // Author: Teoman Ata Korkmaz
+#pragma GCC optimize("Ofast")
+#pragma GCC target("tune=native")
 #include <bits/stdc++.h> 
 #define int int_fast64_t
 using namespace std;
 constexpr int N=2e5+5;
 ///////////////////////////////////////////////////////////
-int n,m,q,x,y,last[N],t,cnt[40000006];
+int n,m,q,x,y,first[N],last[N];
+
+struct Segtree{
+    #define m ((l+r)>>1)
+    #define lc (node<<1)
+    #define rc ((node<<1)|1)
+    int* st;
+    Segtree(int n){
+        st==new int[4*n+5];
+    }
+    inline void push(int l,int r,int node){
+        if(l==r || st[node]==-1)return;
+        st[lc]=st[node];
+        st[rc]=st[node];
+        st[node]=-1;
+    }
+    inline void update(int x,int y,int task,int l=1,int r=n,int node=1){
+        if(r<x || y<l)return;
+        if(x<=l && r<=y){
+            st[node]=task;
+            return;
+        }
+        push(l,r,node);
+        update(x,y,task,l,m,lc);
+        update(x,y,task,m+1,r,rc);
+    }
+    inline int query(int x,int l=1,int r=n,int node=1){
+        if(l==r)return st[node];
+        push(l,r,node);
+        if(x<=m)query(x,l,m,lc);
+        else query(x,m+1,r,rc);
+    }
+    #undef m
+    #undef lc
+    #undef rc
+};
 
 signed main(void){
-    memset(last,-1,sizeof(last));
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin>>n>>m>>q;
-    while(m--){
-        cin>>x>>y;
-        for(int i=x;i<=y;i++,t++){
-            if(last[i]!=-1)cnt[t-last[i]]++;
-            last[i]=t;
+    for(int i=0;i<m;i++)cin>>first[i]>>last[i];
+
+    Segtree st(n);
+    for(int i=0;i<n;i++){
+        int l=first[i],r=last[i];
+
+        if(l>1){
+            int task=st.query(l-1);
+            if(task!=-1)last[task]=l-1;
         }
-    }
+        for(int p=l;p<=r;){
+            int task=st.query(p);
+            
+        }
 
-    int sum=0;
-    for(int i=40000005;i--;)cnt[i]+=cnt[i+1];
-
-    while(q--){
-        cin>>x;
-        if(x>(2+n)*(1+2000+1))cout<<0<<" ";
-        else cout<<cnt[x+1]<<" ";
+        st.update(l,r,i,r);
     }
 }
