aeec8b18c1164438e9bf3bb98c8babe288390596
Teoman Ata Korkmaz <tte0@disroot.org>
2025-11-20 19:13:11 +0300

    .

--- Range_Queries_and_Copies.cpp
+++ Range_Queries_and_Copies.cpp
@@ -1,65 +1,92 @@
 // Author: Teoman Ata Korkmaz
 #include <bits/stdc++.h> 
-#define int int_fast64_t
 using namespace std;
-constexpr int N=200005;
-///////////////////////////////////////////////////////////
-int n,q,x,y,k,cnt;
-vector<int> v;
+constexpr int N=2e5+5;
+struct segtree{
+    int n;
+    vector<int64_t> st;
+    vector<size_t> left,right,root;
+    
+    size_t newNode(int val){
+        st.push_back(val);
+        left.push_back(-1);
+        right.push_back(-1);
+        return st.size()-1;
+    }
+    size_t newNode(int l,int r){
+        st.push_back(st[l]+st[r]);
+        left.push_back(l);
+        right.push_back(r);
+        return st.size()-1;
+    }
+    size_t newNode(size_t node){
+        st.push_back(st[node]);
+        left.push_back(left[node]);
+        right.push_back(right[node]);
+        return st.size()-1;
+    }
 
-struct Node{
-    int val;
-    Node* left;
-    Node* right;
-    Node(int x=0):val(x),left(NULL),right(NULL){}
-    Node(Node* x):val(x->val),left(x->left),right(x->right){}
-    Node(Node* l,Node* r){
-        left=l,right=r,val=0;
-        if(l)val+=l->val;
-        if(r)val+=r->val;
+    segtree(const vector<int>& v){
+        n=v.size();
+        root.push_back(build(1,n,v));
+    }
+
+    #define mid ((l+r)>>1)
+    
+    inline size_t build(int l,int r,const vector<int>& v){
+        if(l==r)return newNode(v[l-1]);
+        return newNode(build(l,mid,v),build(mid+1,r,v));
+    }
+    
+    inline size_t update(int node,int l,int r,int qx,int qy){
+        if(l==r)   return newNode(qy);
+        if(qx<=mid)return newNode(update(left[node],l,mid,qx,qy),right[node]);
+        else       return newNode(left[node],update(right[node],mid+1,r,qx,qy));
+    }
+    
+    inline int64_t query(int node,int l,int r,int ql,int qr){
+        if(r<ql || qr<l)return 0;
+        if(ql<=l && r<=qr)return st[node];
+        return query(left[node],l,mid,ql,qr)+query(right[node],mid+1,r,ql,qr);
+    }
+    #undef mid
+
+    int64_t query(int k,int ql,int qr){
+        return query(root[k],1,n,ql,qr);
+    }
+
+    void update(int k,int qx,int qy){
+        root[k]=update(newNode(root[k]),1,n,qx,qy);
+    }
+
+    void copy(int k){
+        root.push_back(newNode(root[k]));
     }
 };
-
-#define mid (l+r>>1)
-
-inline Node* build(int l=1,int r=n){
-    if(l==r)return new Node(v[l-1]);
-    return new Node(build(l,mid),build(mid+1,r));
-}
-
-inline Node* update(Node* node,int l=1,int r=n){
-    if(l==r)  return new Node(y);
-    if(x<=mid)return new Node(update(node->left,l,mid),node->right);
-    else      return new Node(node->left,update(node->right,mid+1,r));
-}
-
-inline int query(Node* node,int l=1,int r=n){
-    if(r<x || y<l)return 0;
-    if(x<=l && r<=y)return node->val;
-    return query(node->left,l,mid)+query(node->right,mid+1,r);
-}
+///////////////////////////////////////////////////////////
+int n,q,cnt,root[N];
+vector<int> v;
 
 signed main(void){
     cin>>n>>q;
     v.resize(n);
-    for(int& i:v)cin>>i;
-
-    Node* root[q+5];   
-    root[++cnt]=build();
+    for(auto& i:v)cin>>i;
+    
+    segtree st(v);
 
     while(q--){
-        cin>>x;
+        int x,a,b,k;
+        cin>>x>>k;k--;
         if(x==1){
-            cin>>k>>x>>y;
-            root[k]=update(new Node(root[k]));
+            cin>>a>>x;
+            st.update(k,a,x);
         }
         else if(x==2){
-            cin>>k>>x>>y;
-            cout<<query(root[k])<<endl;
+            cin>>a>>b;
+            cout<<st.query(k,a,b)<<endl;
         }
         else{
-            cin>>k;
-            root[++cnt]=new Node(root[k]);
+            st.copy(k);
         }
-    } 
+    }
 }
